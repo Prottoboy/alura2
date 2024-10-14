ad7bf03aed3797d661d0d954ea804e2e2bf17f40
Prottoboy <renandesouzasantoss@gmail.com>
2024-10-14 08:02:59 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pessoal',
+    'Qual é a sua cor favorita? ',
+    'Todas'
+)
+
+criaCartao(
+    'pessoal',
+    'qual Desenho favorito?',
+    'Apenas um show'
+)
+
+criaCartao(
+    'recomendação',
+    'uma música',
+    'Depois do Culto'
+)
+
+criaCartao(
+    'recomendacão',
+    'um personagem',
+    'Wonder of U'
+)
